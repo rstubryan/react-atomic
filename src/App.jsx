c1f76cdd7c79c306eb509af6ff9074aecfc18bfd
restubumirr <restubumirr@gmail.com>
2024-05-17 02:24:00 +0000

    feat(+): use card

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,20 @@ import { Button } from "./components/ui/button";
 function App() {
   return (
     <>
-        <h1 className="text-2xl font-bold underline">Hello World</h1>
-        <Button>Click Me</Button>
+      <h1 className="text-2xl font-bold underline">Hello World</h1>
+      <Button>Click Me</Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </>
   );
 }
