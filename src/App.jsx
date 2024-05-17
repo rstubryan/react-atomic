3841e12e83ada1b34d46327e626011be538d7e4b
restubumirr <restubumirr@gmail.com>
2024-05-17 02:19:08 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+        <h1 className="text-2xl font-bold underline">Hello World</h1>
+        <Button>Click Me</Button>
+    </>
+  );
+}
+
+export default App;
