9d4936a50758795ecee4fb264909d4da7bc56ad5
restubumirr <restubumirr@gmail.com>
2024-05-19 14:45:22 +0000

    feat(+): add react router dom for navigating the pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RegistrationPages from './pages/RegistrationPages.jsx';
+import LoginPages from './pages/LoginPages.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/register",
+    element: <RegistrationPages />,
+  },
+  {
+    path: "/login",
+    element: <LoginPages />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
